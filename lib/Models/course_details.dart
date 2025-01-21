615399a7862c0f62797422c4e45e617cb95cecfd
Abdallah Elshayeb <abdallahelshayeb515@gmail.com>
2025-01-21 20:00:59 +0200

    database collection

--- lib/Models/course_details.dart
+++ lib/Models/course_details.dart
@@ -9,6 +9,31 @@ class CourseDetails {
   CourseDetails(this.courseName, this.imgUrl, this.offerPercent,
       this.instructorName, this.courseFees,
       {this.isFavorite = false});
+
+  // convert a courseDetails object to map
+  Map<String, dynamic> toMap() {
+    return {
+      'courseName': courseName,
+      'imgUrl': imgUrl,
+      'offerPercent': offerPercent,
+      'instructorName': instructorName,
+      'courseFees': courseFees,
+      'isFavorite': isFavorite,
+    };
+  }
+
+  // create a courseDetails object from map
+  factory CourseDetails.fromMap(Map<String, dynamic> map) {
+    return CourseDetails(
+      map['courseName'] as String,
+      map['imgUrl'] as String,
+      map['offerPercent'] as String,
+      map['instructorName'] as String,
+      map['courseFees'] as String,
+      isFavorite: map['isFavorite'] as bool? ?? false,
+    );
+  }
+
   @override
   String toString() {
     return 'CourseDetails(courseName: $courseName, instructorName: $instructorName, courseFees: $courseFees)';
--- lib/provider/courseModel.dart
+++ lib/provider/courseModel.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/foundation.dart';
 import '../Models/course_details.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CourseModel with ChangeNotifier {
   List<CourseDetails> _favoriteCourses = [];
@@ -13,15 +14,65 @@ class CourseModel with ChangeNotifier {
       _favoriteCourses.add(course);
     }
     notifyListeners();
+    // saveToFirebase();
+    getFavoriteCourses();
   }
 
   void removeCourses(CourseDetails course) {
     _favoriteCourses.remove(course);
     notifyListeners();
+    // saveToFirebase();
+    getFavoriteCourses();
   }
 
   void savedCourses(List<CourseDetails> courses) {
     _favoriteCourses = courses;
     notifyListeners();
+    // saveToFirebase();
+    getFavoriteCourses();
+  }
+
+  Future<void> saveToFirebase() async {
+    final db = FirebaseFirestore.instance;
+
+    try {
+      final courseList =
+          _favoriteCourses.map((course) => course.toMap()).toList();
+      // await db
+      //     .collection('favoriteCourses')
+      //     .doc('cebPEoyFomKHdTeZxTZg')
+      //     .set({'favorite_course': courseList});
+      await db
+          .collection("favoriteCourses")
+          .add({'favorite_course': courseList});
+      print('course Details saved successfully!!');
+    } catch (e) {
+      print('failled to save course details: $e');
+    }
+  }
+
+  Future<void> getFavoriteCourses() async {
+    final db = FirebaseFirestore.instance;
+    try {
+      final favouriteCourses = await db
+          .collection("favoriteCourses")
+          .doc('UFzz3byNKwwKWreNK0S0')
+          .get();
+      if (favouriteCourses.exists && favouriteCourses.data() != null) {
+        final data = favouriteCourses.data();
+        // final List<dynamic> courses = data['favorite_courses'];
+        final List<dynamic> courses = data?['favorite_courses'] ?? [];
+        _favoriteCourses = courses
+            .map((course) =>
+                CourseDetails.fromMap(course as Map<String, dynamic>))
+            .toList();
+
+        notifyListeners();
+        print('courses get successfully!!');
+        print(data);
+      }
+    } catch (e) {
+      print('failed to get courses: $e');
+    }
   }
 }
